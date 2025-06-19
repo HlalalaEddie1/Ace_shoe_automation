518c53008815ae8eab8abe29c43cb93b9e02636f
ENgoveni <eddyngoveni@gmail.com>
2025-06-19 11:36:39 +0200

    com

--- src/test/java/pages/LoggedInPage.java
+++ src/test/java/pages/LoggedInPage.java
@@ -12,9 +12,11 @@ import static org.junit.Assert.assertEquals;
 public class LoggedInPage extends BrowserDriver {
 
     public static String productcategory_formalshoes= "//*[text()='Formal Shoes']";
-    public static String productcategory_sportsshoes= "//*[text()='Sports Shoes']";
+    public static String productcategory_sportsshoes= "//*[text()='Sports']";
+    public static String productcategory_sneakers = "//*[text()='Sneakers']";
 
-    public static String productcategory_sneakers= "//*[text()='Sneakers Shoes']";
+
+    //public static String productcategory_sneakers= "//*[text()='Sneakers Shoes']";
 
 
     public static void visibility_productcategory_formalshoes() throws InterruptedException{
@@ -25,13 +27,15 @@ public class LoggedInPage extends BrowserDriver {
     public static void visibility_productcategory_sportsshoes() throws InterruptedException{
 
         String actualproductcategory_ss = driver.findElement(By.xpath(productcategory_sportsshoes)).getText();
-        assertEquals("Sports Shoes",actualproductcategory_ss);
+        assertEquals("Sports",actualproductcategory_ss);
 
     }
 
     public static void visibility_productcategory_sneakershoes() {
         String actualproductcategory_s = driver.findElement(By.xpath(productcategory_sneakers)).getText();
-        assertEquals("Sneakers",actualproductcategory_s);
+        //assertEquals("Sneakers",actualproductcategory_s)
+        assertEquals("Sneakers", actualproductcategory_s);
+        ;
 
 
     }
